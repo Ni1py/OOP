11abb1eb8a6ee8340f395d4d0b533c4a9275fdef
Ni1py <gekepyrkov@gmail.com>
2022-05-15 17:40:34 +0300

    Добавил задание из примера

---
+++ 3_lab/GlassWithTests/GlassWithTests/CGlass.h
@@ -0,0 +1,25 @@
+#pragma once
+
+class CGlass
+{
+public:
+	// создаем стакан заданной вместимости
+	CGlass(int capacity);
+
+	~CGlass();
+
+	// возвращаем вместимость стакана
+	int GetCapacity() const;
+
+	// возвращаем количество воды в стакане
+	int GetWaterAmount() const;
+
+	// добавляем воду в стакан, возвращая фактически добавленное количество
+	int AddWater(int amount);
+
+	// выливаем воду из стакана, возвращаем фактически вылитое количество
+	int PourWater(int amount);
+private:
+	int m_capacity;
+	int m_waterAmount;
+};
